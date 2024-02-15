cf143068f10546822de155a95eace528cc461042
Jarho <christopherdezmond@gmail.com>
2024-02-15 23:03:16 +0100

    corrected

--- shell_llop.c
+++ shell_llop.c
@@ -202,15 +202,6 @@ int hsh(info_t *info, char **av)
 	return (builtin_ret);
 }
 
-/**
- * find_builtin - finds a builtin command
- * @info: the parameter & return info struct
- *
- * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
- */
 int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
@@ -236,12 +227,6 @@ int find_builtin(info_t *info)
 	return (built_in_ret);
 }
 
-/**
- * find_cmd - finds a command in PATH
- * @info: the parameter & return info struct
- *
- * Return: void
- */
 void find_cmd(info_t *info)
 {
 	char *path = NULL;
